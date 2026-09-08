9745f4e255bacff3202ded9dff9b0f52b62f1675
Imanshadowww <imanshadowww@atomicmail.io>
2026-09-08 07:23:12 +0330

    Remove
    Removed the download step for Tailscale.

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Downloading Tailscale..."
-curl -fsSL https://pkgs.tailscale.com/stable/tailscale_1.74.0_amd64.tgz | tar xz
-
 echo "Starting tailscaled in userspace mode..."
 ./tailscale_1.74.0_amd64/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 sleep 3
