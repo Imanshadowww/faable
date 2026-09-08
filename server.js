e96c3c0646ce2935c57dca37d5af8ec2bdaae880
Imanshadowww <imanshadowww@atomicmail.io>
2026-09-08 07:23:48 +0330

    Delete server.js

--- server.js
+++
@@ -1,4 +0,0 @@
-const http = require('http');
-const port = process.env.PORT || 8080;
-http.createServer((req, res) => res.end('Tailscale is running in the background!')).listen(port);
-console.log(`Dummy server listening on port ${port}`);
